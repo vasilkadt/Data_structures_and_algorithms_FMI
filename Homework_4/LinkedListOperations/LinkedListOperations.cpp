e2f8512f85f8fb514b8b57b8495744e1a34766ba
vasilkadt <96419451+vasilkadt@users.noreply.github.com>
2022-11-08 22:01:38 +0200

    Add linked list implementation

---
+++ Homework_4/LinkedListOperations/LinkedListOperations.cpp
@@ -0,0 +1,249 @@
+﻿#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+public:
+	int data;
+	SinglyLinkedListNode* next;
+
+	SinglyLinkedListNode(int node_data) {
+		this->data = node_data;
+		this->next = nullptr;
+	}
+};
+
+class SinglyLinkedList {
+public:
+	SinglyLinkedListNode* head;
+	SinglyLinkedListNode* tail;
+
+	SinglyLinkedList() {
+		this->head = nullptr;
+		this->tail = nullptr;
+	}
+
+	void insert_node(int node_data) {
+		SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+		if (!this->head) {
+			this->head = node;
+		}
+		else {
+			this->tail->next = node;
+		}
+
+		this->tail = node;
+	}
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+	while (node) {
+		fout << node->data;
+
+		node = node->next;
+
+		if (node) {
+			fout << sep;
+		}
+	}
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+	while (node) {
+		SinglyLinkedListNode* temp = node;
+		node = node->next;
+
+		free(temp);
+	}
+}
+
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* llist, int data, int position)
+{
+	if (position == 0)
+	{
+		SinglyLinkedListNode* newHead = new SinglyLinkedListNode(data);
+		newHead->next = llist;
+		llist = newHead;
+		return llist;
+	}
+	else if (llist == nullptr)
+	{
+		throw out_of_range("Invalid insert index. list is empty");
+	}
+	else
+	{
+		SinglyLinkedListNode* current = llist;
+		SinglyLinkedListNode* previous = nullptr;
+		for (int i = 0; i < position; i++)
+		{
+			if (current->next != nullptr)
+			{
+				previous = current;
+				current = current->next;
+			}
+			else
+			{
+				throw out_of_range("Cannot insert in invalid position");
+			}
+		}
+		previous->next = new SinglyLinkedListNode(data);
+		previous->next->next = current;
+	}
+	return llist;
+}
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position)
+{
+	if (position == 0)
+	{
+		SinglyLinkedListNode* current = llist;
+		llist = llist->next;
+		delete current;
+		return llist;
+	}
+	else if (llist == nullptr)
+	{
+		throw out_of_range("Invalid insert index. list is empty");
+	}
+	else
+	{
+		SinglyLinkedListNode* current = llist;
+		SinglyLinkedListNode* previous = nullptr;
+		for (int i = 0; i < position; i++)
+		{
+			if (current->next != nullptr)
+			{
+				previous = current;
+				current = current->next;
+			}
+			else
+			{
+				throw out_of_range("Cannot insert in invalid position");
+			}
+		}
+		previous->next = current->next;
+		delete current;
+	}
+	return llist;
+}
+
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2)
+{
+	int firstLenght = 0;
+	int secondLenght = 0;
+
+	SinglyLinkedListNode* temp1 = head1;
+	SinglyLinkedListNode* temp2 = head2;
+	while (head1 != nullptr)
+	{
+		firstLenght++;
+		head1 = head1->next;
+	}
+
+	while (head2 != nullptr)
+	{
+		secondLenght++;
+		head2 = head2->next;
+	}
+
+	int meetPoint = 0;
+	int startPoint = abs(firstLenght - secondLenght);
+	if (firstLenght > secondLenght)
+	{
+		for (int i = 0; i < startPoint; i++)
+		{
+			temp1 = temp1->next;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < startPoint; i++)
+		{
+			temp2 = temp2->next;
+		}
+	}
+
+	while (temp1 != nullptr)
+	{
+		if (temp1 == temp2)
+		{
+			meetPoint = temp1->data;
+			break;
+		}
+		temp1 = temp1->next;
+		temp2 = temp2->next;
+	}
+	return meetPoint;
+}
+
+int main()
+{
+	ofstream fout(getenv("OUTPUT_PATH"));
+
+	int tests;
+	cin >> tests;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+		int index;
+		cin >> index;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		SinglyLinkedList* llist1 = new SinglyLinkedList();
+
+		int llist1_count;
+		cin >> llist1_count;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		for (int i = 0; i < llist1_count; i++) {
+			int llist1_item;
+			cin >> llist1_item;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+			llist1->insert_node(llist1_item);
+		}
+
+		SinglyLinkedList* llist2 = new SinglyLinkedList();
+
+		int llist2_count;
+		cin >> llist2_count;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		for (int i = 0; i < llist2_count; i++) {
+			int llist2_item;
+			cin >> llist2_item;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+			llist2->insert_node(llist2_item);
+		}
+
+		SinglyLinkedListNode* ptr1 = llist1->head;
+		SinglyLinkedListNode* ptr2 = llist2->head;
+
+		for (int i = 0; i < llist1_count; i++) {
+			if (i < index) {
+				ptr1 = ptr1->next;
+			}
+		}
+
+		for (int i = 0; i < llist2_count; i++) {
+			if (i != llist2_count - 1) {
+				ptr2 = ptr2->next;
+			}
+		}
+
+		ptr2->next = ptr1;
+
+		int result = findMergeNode(llist1->head, llist2->head);
+
+		fout << result << "\n";
+	}
+
+	fout.close();
+
+	return 0;
+}
---
+++ Linked list/LinkedListImplementation/LinkedListImplementation/LinkedListImplementation.cpp
@@ -0,0 +1,267 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int value;
+	Node* next;
+
+	Node(int value)
+	{
+		this->value = value;
+		next = nullptr;
+	}
+
+	Node(int value, Node* next)
+	{
+		this->value = value;
+		this->next = next;
+	}
+};
+
+class List
+{
+private:
+	Node* head;
+	Node* tail;
+
+public:
+
+	//default constructor
+	List()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+
+	//destructor
+	~List()
+	{
+		Node* cur = head;
+		Node* prev = nullptr;
+		while (cur != nullptr)
+		{
+			prev = cur->next;
+			delete cur;
+			cur = prev;
+		}
+	}
+
+	//insert element at the end
+	void push_back(int element)
+	{
+		if (head == nullptr)
+		{
+			head = new Node(element, nullptr);
+			tail = head;
+		}
+		else
+		{
+			tail->next = new Node(element, nullptr);
+			tail = tail->next;
+		}
+	}
+
+	//insert element at the beginning
+	void push_front(int element)
+	{
+		Node* newHead = new Node(element, head);
+		head = newHead;
+		if (tail == nullptr) 
+		{
+			tail = head;
+		}
+	}
+
+	//print elements
+	void print() const
+	{
+		Node* current = head;
+		while (current != nullptr)
+		{
+			cout << current->value << " ";
+			current = current->next;
+		}
+		cout << "\n";
+	}
+
+	//insert element at position
+	void insert(int element, int position)
+	{
+		if (position == 0)
+		{
+			push_front(element);
+			return;
+		}
+		else if (head == nullptr)
+		{
+			throw out_of_range("Invalid insert index. list is empty");
+		}
+		else
+		{
+			Node* current = head;
+			Node* previous = nullptr;
+			for (int i = 0; i < position; i++)
+			{
+				if (current->next != nullptr)
+				{
+					previous = current;
+					current = current->next;
+				}
+				else
+				{
+					throw out_of_range("Cannot insert in invalid position");
+				}
+			}
+			previous->next = new Node(element, current);
+			if (tail == previous)
+			{
+				tail = previous->next;
+			}
+		}
+	}
+
+	//delete first element
+	void pop_front()
+	{
+		Node* current = head;
+		head = head->next;
+		delete current;
+	}
+
+	//delete last element
+	void pop_back()
+	{
+		Node* current = head;
+		Node* previous = nullptr;
+		while (current->next != nullptr)
+		{
+			previous = current;
+			current = current->next;
+		}
+		tail = previous;
+		previous->next = nullptr;
+		delete current;
+	}
+
+	//remove element at position
+	void remove(int position)
+	{
+		if (position == 0)
+		{
+			pop_front();
+			return;
+		}
+		else if (head == nullptr)
+		{
+			throw out_of_range("Invalid remove index, list is empty");
+		}
+		else
+		{
+			Node* current = head;
+			Node* previous = nullptr;
+			for (int i = 0; i < position; i++)
+			{
+				if (current->next != nullptr)
+				{
+					previous = current;
+					current = current->next;
+				}
+				else
+				{
+					throw out_of_range("Cannot insert in invalid position");
+				}
+			}
+			previous->next = current->next;
+			if (current == tail)
+			{
+				tail = previous;
+			}
+			delete current;
+		}
+	}
+
+	//search element in list
+	bool search(int element)
+	{
+		Node* current = head;
+		while (current->next != nullptr)
+		{
+			if (current->value == element)
+			{
+				return true;
+			}
+			current = current->next;
+		}
+		return false;
+	}
+
+	//return first element of the list
+	int peek_front() {
+		return head->value;
+	}
+
+	//return last element of the list
+	int peek_back() {
+		return tail->value;
+	}
+
+	//reverse list
+	void reverse()
+	{
+		Node* current, * previous, * nextNode;
+		current = head;
+		previous = nullptr;
+		nextNode = current->next;
+
+		while (nextNode != nullptr)
+		{
+			current->next = previous;
+			previous = current;
+			current = nextNode;
+			nextNode = nextNode->next;
+		}
+		current->next = previous;
+		head->next = nullptr;
+
+		swap(head, tail);
+	}
+
+	//clear the list
+	void clear()
+	{
+		if (head == nullptr)
+		{
+			return;
+		}
+
+		Node* previous = head;
+		Node* currrent = head->next;
+
+		while (currrent != nullptr) {
+			delete previous;
+
+			previous = currrent;
+			currrent = currrent->next;
+		}
+
+		delete previous;
+	}
+};
+
+int main()
+{
+	List list;
+
+	list.push_front(1);
+	list.push_back(5);
+	list.insert(4, 1);
+	list.insert(19, 2);
+	list.print();
+	list.reverse();
+	list.print();
+	list.remove(1);
+	list.print();
+	list.pop_back();
+	list.print();
+}
